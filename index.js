21ec78e46c86a0abde7f651e442cff4995c49d90
yuki-uchida <rantarn0326@gmail.com>
2021-06-29 18:33:39 +0900

    revert

--- index.js
+++ index.js
@@ -1,52 +1,41 @@
 const canvasElement = document.getElementById("output_canvas");
-// ストリームの取得
-const segmentedLocalMediaStream = canvasElement.captureStream(); // 25 FPS
-const worker = new Worker("offscreen_worker.js");
-const offscreenCanvas = canvasElement.transferControlToOffscreen();
-
-async function loadImage() {
-  return new Promise((resolve, reject) => {
-    const backgroundImage = new Image();
-    backgroundImage.onload = () => resolve(backgroundImage);
-    backgroundImage.onerror = (e) => reject(backgroundImage);
-    backgroundImage.src = "./photo-1579488083677-ac53dcf66911.jpeg";
-  });
-}
-
-(async function () {
-  const backgroundImage = await loadImage();
-  const backgroundImageBitMap = await createImageBitmap(backgroundImage);
-  worker.postMessage(
-    {
-      msg: "offscreen",
-      canvas: offscreenCanvas,
-      backgroundImageBitMap: backgroundImageBitMap,
-    },
-    [offscreenCanvas, backgroundImageBitMap]
+const canvasCtx = canvasElement.getContext("2d");
+const chara = new Image();
+chara.src = "./sample.jpeg";
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+    results.segmentationMask,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
   );
-  function onResults(results) {
-    worker.postMessage(
-      {
-        msg: "imageBitMap",
-        results: results,
-      },
-      results
-    );
-    results.segmentationMask.close();
-    results.image.close();
-  }
-  const selfieSegmentation = new SelfieSegmentation({
-    locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
-    },
-  });
-  selfieSegmentation.setOptions({
-    modelSelection: 1,
-  });
-  selfieSegmentation.onResults(onResults);
-
-  //////////////////////////////////////////////////////
-
+  canvasCtx.globalCompositeOperation = "source-in";
+  canvasCtx.drawImage(
+    results.image,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
+  );
+  canvasCtx.globalCompositeOperation = "destination-atop";
+  canvasCtx.drawImage(chara, 0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.restore();
+  results.segmentationMask.close();
+  results.image.close();
+}
+const selfieSegmentation = new SelfieSegmentation({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
+  },
+});
+selfieSegmentation.setOptions({
+  modelSelection: 1,
+});
+selfieSegmentation.onResults(onResults);
+(async function () {
   const localMediaStream = await navigator.mediaDevices.getUserMedia({
     video: {
       width: 1080,
@@ -73,9 +62,12 @@ async function loadImage() {
     },
   });
   processor.readable.pipeTo(writable);
-
+  // キャプチャしたい canvas 要素を取得
+  const canvasElt = document.getElementById("output_canvas");
+  // ストリームの取得
+  const segmentedLocalMediaStream = canvasElt.captureStream(); // 25 FPS
   const peer = new Peer({
-    key: "YOUR API KEY",
+    key: "<YOUR API KEY>",
     debug: 3,
   });
   peer.on("open", () => {
